faeb0db8eef2433f5ba9ffb280b52a60106b1285
dannielccw <dannielccw@gmail.com>
2017-03-06 11:12:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/s030601/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.yvtc.s030601;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    final int REQUEST_CODE_CAMERA = 321;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CODE_CAMERA);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_CAMERA)
+        {
+            if (resultCode == RESULT_OK) {
+                if(data !=null ) {
+                    //返回有縮略圖
+                    if(data.hasExtra("data")) {
+                        Bitmap thumbnail = data.getParcelableExtra("data");
+                        //得到bitmap後的操作
+                        ImageView img = (ImageView) findViewById(R.id.imageView);
+                        img.setImageBitmap(thumbnail);
+                    }
+                    else {
+                        //由于指定了目标uri，存储在目标uri，intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                        // 通过目标uri，找到图片
+                        // 对图片的缩放处理
+                        // 操作
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+}
